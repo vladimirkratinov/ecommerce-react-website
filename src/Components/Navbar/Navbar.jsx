26a5da7d6b9abdbfbf293ebb96090554db667343
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-21 21:49:51 -0800

    initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      
+    </div>
+  )
+}
