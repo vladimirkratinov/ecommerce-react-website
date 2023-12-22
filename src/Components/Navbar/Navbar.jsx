9cec7f5086fcc38030c480a97303105e53476097
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-21 22:02:56 -0800

    configure navbar + css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop <hr/></li>
+        <li>Men</li>
+        <li>Woman</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-  )
+  );
 }
