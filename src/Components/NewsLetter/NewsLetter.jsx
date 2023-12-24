0449649464b777fe4184d8bc91b613811e8765fd
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-23 23:04:53 -0800

    add NewsLetter.jsx + css

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,7 +4,14 @@ import "./NewsLetter.css";
 const NewsLetter = () => {
   return (
     <div>
-      <div className="newsletter"></div>
+      <div className="newsletter">
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newsleterr and stay updated</p>
+        <div>
+          <input type="email" placeholder="Your Email Address" />
+          <button>Subscribe</button>
+        </div>
+      </div>
     </div>
   );
 };
