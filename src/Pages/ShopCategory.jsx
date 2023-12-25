69f89ffb280da4c60ffcebbd8f7dc85cf9b2fc7b
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-24 19:17:26 -0800

    add customizable banner to ShopCategory

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_products} = useContext(ShopContext);
   return (
     <div className='shop-category'>
-      
+      <img src={props.banner} alt="" />
     </div>
   )
 }
