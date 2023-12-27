9e2e90271b21abf4a98ec9e3456136a94586a442
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-26 17:36:21 -0800

    setup Breadcrum.jsx/css

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Breadcrum.css'
+
+const Breadcrum = () => {
+  return (
+    <div>
+      <div className="breadcrum">
+
+      </div>
+    </div>
+  )
+}
+
+export default Breadcrum
