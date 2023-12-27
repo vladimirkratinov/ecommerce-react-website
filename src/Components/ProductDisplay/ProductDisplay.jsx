c84a561b43ef79f61ec026e9175a0cbd421ee58d
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-26 18:07:27 -0800

    configure ProductDisplay.jsx

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import './ProductDisplay.css'
+import React from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
 
-const ProductDisplay = () => {
+const ProductDisplay = (props) => {
+  const { product } = props;
   return (
-    <div className='product-display'>
-      
+    <div className="product-display">
+      <div className="product-display-left">
+        <div className="product-display-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="product-display-img">
+          <img
+            className="product-display-main-img"
+            src={product.image}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="product-display-right">
+        <h1>{product.name}</h1>
+        <div className="product-display-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="product-display-right-prices">
+          <div className="product-display-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="product-display-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="product-display-right-description">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+        <div className="product-display-right-size">
+          <h1>Select Size</h1>
+          <div className="product-display-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className="product-display-right-category">
+          <span>Category: </span>Women, T-Shirt, Crop-Top
+        </p>
+        <p className="product-display-right-category">
+          <span>Tags: </span>Modern, Latest, Winter
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
