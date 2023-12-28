fb77d13483d33d6901a4d3e08ab3ef51fb769d89
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-27 20:50:58 -0800

    setup RelatedProducts.jsx/css

---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RelatedProducts.css'
+
+const RelatedProducts = () => {
+  return (
+    <div className='related-products'>
+      
+    </div>
+  )
+}
+
+export default RelatedProducts
