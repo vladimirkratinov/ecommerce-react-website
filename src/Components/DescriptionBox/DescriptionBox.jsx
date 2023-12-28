aff1ed4b7c86b029e1b2d68c6aad105570148490
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-27 20:44:31 -0800

    configure DescriptionBox.jsx

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './DescriptionBox.css'
+import React from "react";
+import "./DescriptionBox.css";
 
 const DescriptionBox = () => {
   return (
-    <div className='description-box'>
-      
+    <div className="description-box">
+      <div className="description-box-navigator">
+        <div className="description-box-nav-box">Description</div>
+        <div className="description-box-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="description-box-description">
+        <p>
+          Sure, here's a generic description for a T-shirt: Product Name:
+          Classic Cotton Crewneck T-shirt Description: Elevate your casual
+          wardrobe with our Classic Cotton Crewneck T-shirt. Crafted from
+          high-quality, breathable cotton, this timeless piece offers comfort
+          and style in one. The crewneck design provides a versatile look that
+          effortlessly pairs with jeans, shorts, or your favorite casual
+          bottoms. The soft and lightweight fabric ensures all-day comfort,
+          making it an ideal choice for everyday wear. Whether you're heading to
+          a weekend brunch or simply relaxing at home, this T-shirt is a
+          wardrobe essential. Choose from a range of classic colors to suit your
+          personal style and make a statement with a wardrobe staple that never
+          goes out of fashion.
+        </p>
+        <p>
+          
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DescriptionBox
+export default DescriptionBox;
