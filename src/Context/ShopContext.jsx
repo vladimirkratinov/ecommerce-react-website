32c0f476ce0542cde10518f892059841e3cf2799
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-27 21:14:48 -0800

    configure ShopContext (Cart) Logic

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -19,6 +19,7 @@ const ShopContextProvider = (props) => {
   const addToCart = (itemId) => {
     //provide value for key
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log(cartItems);
   };
 
   const removeFromCart = (itemId) => {
