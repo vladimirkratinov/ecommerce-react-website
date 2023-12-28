2bb5ffcca204d3743f0804848f27410a802ec427
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-27 21:17:02 -0800

    setup CartItems.jsx/css

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CartItems.css'
+
+const CartItems = () => {
+  return (
+    <div className='cart-items'>
+      
+    </div>
+  )
+}
+
+export default CartItems
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,40 +1,12 @@
-// Cart.jsx
-
-import React from 'react';
-import './CSS/Cart.css';
+import React from 'react'
+import CartItems from '../Components/CartItems/CartItems'
 
 const Cart = () => {
-  const cartItems = [
-    { id: 1, name: 'Product A', price: 20, quantity: 2 },
-    { id: 2, name: 'Product B', price: 30, quantity: 1 },
-    // Add more items as needed
-  ];
-
   return (
-    <div className="cart-container">
-      <h2>Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <>
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.id}>
-                <span>{item.name}</span>
-                <span>Quantity: {item.quantity}</span>
-                <span>Price: ${item.price * item.quantity}</span>
-                <button>Remove</button>
-              </li>
-            ))}
-          </ul>
-          <div className="total">
-            <p>Total: $100</p>
-            <button>Checkout</button>
-          </div>
-        </>
-      )}
+    <div>
+      <CartItems />
     </div>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
