55fd648b5f41e00458a1d4e1b5ab30c651e03638
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-27 20:49:49 -0800

    configure DiscriptionBox.css

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -10,22 +10,18 @@ const DescriptionBox = () => {
       </div>
       <div className="description-box-description">
         <p>
-          Sure, here's a generic description for a T-shirt: Product Name:
-          Classic Cotton Crewneck T-shirt Description: Elevate your casual
-          wardrobe with our Classic Cotton Crewneck T-shirt. Crafted from
-          high-quality, breathable cotton, this timeless piece offers comfort
-          and style in one. The crewneck design provides a versatile look that
-          effortlessly pairs with jeans, shorts, or your favorite casual
-          bottoms. The soft and lightweight fabric ensures all-day comfort,
-          making it an ideal choice for everyday wear. Whether you're heading to
-          a weekend brunch or simply relaxing at home, this T-shirt is a
-          wardrobe essential. Choose from a range of classic colors to suit your
-          personal style and make a statement with a wardrobe staple that never
-          goes out of fashion.
+          Elevate your casual wardrobe with our Classic Cotton Crewneck T-shirt.
+          Crafted from high-quality, breathable cotton, this timeless piece
+          offers comfort and style in one. The crewneck design provides a
+          versatile look that effortlessly pairs with jeans, shorts, or your
+          favorite casual bottoms. The soft and lightweight fabric ensures
+          all-day comfort, making it an ideal choice for everyday wear. Whether
+          you're heading to a weekend brunch or simply relaxing at home, this
+          T-shirt is a wardrobe essential. Choose from a range of classic colors
+          to suit your personal style and make a statement with a wardrobe
+          staple that never goes out of fashion.
         </p>
-        <p>
-          
-        </p>
+        <p></p>
       </div>
     </div>
   );
