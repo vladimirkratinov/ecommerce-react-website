c9a7f8e12133b024b8bac98811f19757ce217259
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-30 23:37:51 -0800

    update Sign Up page

--- src/Pages/LoginSignupPage.jsx
+++ src/Pages/LoginSignupPage.jsx
@@ -1,1 +1,29 @@
+import React from "react";
+import "./CSS/LoginSignupPage.css";
 
+const LoginSignupPage = () => {
+  return (
+    <div>
+      <div className="login-signup">
+        <div className="login-signup-container">
+          <h1>Sign Up</h1>
+          <div className="login-signup-fields">
+            <input type="text" placeholder="Your Name" />
+            <input type="email" placeholder="Email Address" />
+            <input type="password" placeholder="Password" />
+          </div>
+          <button>Continue</button>
+          <p className="login-signup-login">
+            Already have an account?<span>Login here</span>
+          </p>
+          <div className="login-signup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, I agree to the terms of Use & Privacy policy</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignupPage;
